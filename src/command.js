c87ebc887f396a0d8148b88e1dd6588d11477c97
Salah Mohamed <salah.mohamed0404@gmail.com>
2026-02-04 14:57:08 +0200

    feat: init command structure for note management with yargs

--- src/command.js
+++ src/command.js
@@ -3,11 +3,78 @@ import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
 	.command(
-		"curl <url>",
-		"fetch som",
+		"new <note>",
+		"create a new note",
+		(yargs) => {
+			return yargs.positional("note", {
+				describe: "The content of the note you want to create",
+				type: "string",
+			});
+		},
+		async (argv) => {
+			console.log("argv", argv);
+		},
+	)
+	.option("tags", {
+		alias: "t",
+		type: "string",
+		description: "tags to add to the note",
+	})
+	.command(
+		"all",
+		"get all notes",
 		() => {},
-		(argv) => {
-			console.log(argv);
+		async (argv) => {
+			console.log("argv", argv);
+		},
+	)
+	.command(
+		"find <filter>",
+		"get matching notes",
+		(yargs) => {
+			return yargs.positional("filter", {
+				describe:
+					"The search term to filter notes by, will be applied to note.content",
+				type: "string",
+			});
+		},
+		async (argv) => {
+			console.log("argv", argv);
+		},
+	)
+	.command(
+		"remove <id>",
+		"remove a note by id",
+		(yargs) => {
+			return yargs.positional("id", {
+				type: "number",
+				description: "The id of the note you want to remove",
+			});
+		},
+		async (argv) => {
+			console.log("argv", argv);
+		},
+	)
+	.command(
+		"web [port]",
+		"launch website to see notes",
+		(yargs) => {
+			return yargs.positional("port", {
+				describe: "port to bind on",
+				default: 5000,
+				type: "number",
+			});
+		},
+		async (argv) => {
+			console.log("argv", argv);
+		},
+	)
+	.command(
+		"clean",
+		"remove all notes",
+		() => {},
+		async (argv) => {
+			console.log("argv", argv);
 		},
 	)
 	.demandCommand(1)
