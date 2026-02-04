a075deb3a7a395b483cf6d5bcdd40128da05e582
Salah Mohamed <salah.mohamed0404@gmail.com>
2026-02-04 22:12:50 +0200

    feat: init the db json file storage

---
+++ src/db.js
@@ -0,0 +1,24 @@
+import fs from "node:fs/promises";
+
+const DB_PATH = new URL("../db.json", import.meta.url).pathname;
+
+export const getDB = async () => {
+	const db = await fs.readFile(DB_PATH, "utf-8");
+
+	return JSON.parse(db);
+};
+
+export const saveDb = async (db) => {
+	await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
+
+	return db;
+};
+
+export const insertDb = async (note) => {
+	const db = await getDB();
+
+	db.notes.push(note);
+	await saveDb(db);
+
+	return note;
+};
